5ad1b8ae7594213b68bf39f0a394625b7f931301
anand16158 <168071703+anand16158@users.noreply.github.com>
2026-01-13 11:36:45 +0530

    Update main.c

--- baremetal/main.c
+++ baremetal/main.c
@@ -1,15 +1,26 @@
 #include "core/interrupt.h"
 #include "core/event_buffer.h"
+#include "drivers/gpio.h"
+#include "drivers/timer.h"
+#include "drivers/watchdog.h"
 
 int main(void)
 {
     interrupt_init();
 
+    gpio_init();
+    gpio_enable_irq(0);
+
+    timer_init(1000);
+    watchdog_init(5000);
+
     arm_event_t evt;
 
     while (1) {
+        watchdog_kick();
+
         if (event_read(&evt) == 0) {
-            // Placeholder for debug / LED / trace
+            // Event consumed
             (void)evt;
         }
     }
