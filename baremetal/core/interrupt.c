e69b4be7d5af932a14b0f34317b50c6e29ac90ad
anand16158 <168071703+anand16158@users.noreply.github.com>
2026-01-13 11:29:55 +0530

    Create interrupt.c

---
+++ baremetal/core/interrupt.c
@@ -0,0 +1,32 @@
+#include "interrupt.h"
+#include "event_buffer.h"
+
+#define IRQ_GPIO     5
+#define IRQ_TIMER    10
+#define IRQ_WATCHDOG 15
+
+void interrupt_init(void)
+{
+    event_buffer_init();
+}
+
+void irq_dispatch(uint32_t irq)
+{
+    switch (irq) {
+    case IRQ_GPIO:
+        event_log(EVENT_SRC_GPIO, 1);
+        break;
+
+    case IRQ_TIMER:
+        event_log(EVENT_SRC_TIMER, 0);
+        break;
+
+    case IRQ_WATCHDOG:
+        event_log(EVENT_SRC_WATCHDOG, 2);
+        break;
+
+    default:
+        event_log(EVENT_SRC_FAULT, 3);
+        break;
+    }
+}
